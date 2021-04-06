70f70fd27025e5df8904e6c5b6af0248415c99a6
glendo <gleneggers64@gmail.com>
2021-04-06 16:25:51 -0500

    add renderLicenseBadge, renderLicenseLink, and renderLicenseSection functions; add return markdown text in generateMarkdown function

--- generate-markdown.js
+++ generate-markdown.js
@@ -1,20 +1,58 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {};
+function renderLicenseBadge(license) {
+  if (license === "No License") {
+    return "";
+  }
+
+  return `[![license badge](https://img.shields.io/badge/license-${license}-brightgreen)](${renderLicenseLink(license)})`
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch(license) {
+    case "MIT":
+      return "https://opensource.org/licenses/MIT";
+    case "Apache":
+      return "https://opensource.org/licenses/Apache-2.0";
+    case "GPL":
+      return "https://opensource.org/licenses/gpl-license";
+  }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "No License") {
+    return "";
+  }
+  return `## License
+Licensed under the [${license}](${renderLicenseLink(license)}) license.`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-`;
+${renderLicenseBadge(data.license)}
+## Description
+${data.description}
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Questions](#questions)
+${(data.license !== "No License") ? "- [License](#license)" : ""}
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## Contributing
+${data.contributing}
+## Questions
+[My GitHub Site](https://www.github.com/${data.username})\\
+If you have any additional questions or concerns, please contact me by email at <${data.email}>.
+${renderLicenseSection(data.license)}`;
 }
 
 module.exports = generateMarkdown;
